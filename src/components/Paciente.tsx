c50294b81f12e82cb49d9928065627cdb58f83a9
Arturo Terceros <toor@Arturos-iMac-Pro.local>
2023-06-15 16:50:47 -0700

    agregando botones de editar y elimanar a paciente

--- src/components/Paciente.tsx
+++ src/components/Paciente.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import React from 'react';
 import {paciente} from './Formulario';
 
@@ -25,6 +25,14 @@ const Paciente = ({item}: PacienteProps) => {
       <Text style={styles.label}>Paciente:</Text>
       <Text style={styles.texto}>{paciente}</Text>
       <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
+      <View style={styles.contenedorBotones}>
+        <Pressable style={[styles.btn, styles.btnEditar]}>
+          <Text style={styles.btnTexto}>Editar</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.btnEliminar]}>
+          <Text style={styles.btnTexto}>Eliminar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -47,6 +55,30 @@ const styles = StyleSheet.create({
   },
   fecha: {
     color: '#374151',
+  },
+  contenedorBotones: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  btn: {
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    minWidth: 100,
+  },
+  btnEditar: {
+    backgroundColor: '#f59e0b',
+  },
+  btnEliminar: {
+    backgroundColor: '#EF4444',
+  },
+  btnTexto: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#fff',
   },
 });
 
